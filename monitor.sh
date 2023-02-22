398fe4d640d141e904a652966993547105c997a4
Dariusz Kowalczyk <darton@users.noreply.github.com>
2023-02-22 11:48:29 +0100

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
 function poweroff {
-        echo 'standby 0.0.0.0' | cec-client -s -d 1 /dev/cec0
-        echo 'standby 0.0.0.0' | cec-client -s -d 1 /dev/cec1
+        echo 'standby 0' | cec-client -s -d 1 /dev/cec0
+        sleep 1
+        echo 'standby 0' | cec-client -s -d 1 /dev/cec1
 }
 
 function poweron {
-        echo 'on 0.0.0.0' | cec-client -s -d 1 /dev/cec0
-        echo 'on 0.0.0.0' | cec-client -s -d 1 /dev/cec1
+        echo 'on 0' | cec-client -s -d 1 /dev/cec0
+        sleep 10
+        echo 'as' | cec-client -s -d 1 /dev/cec0
+        sleep 2
+        echo 'on 0' | cec-client -s -d 1 /dev/cec1
+        sleep 10
+        echo 'as' | cec-client -s -d 1 /dev/cec0
 }
 
 function status {
-        echo 'pow 0.0.0.0' | cec-client -s -d 1 /dev/cec0
-        echo 'pow 0.0.0.0' | cec-client -s -d 1 /dev/cec1
+        echo 'pow 0' | cec-client -s -d 1 /dev/cec0 | grep -v ERROR
+        sleep 1
+        echo 'pow 0' | cec-client -s -d 1 /dev/cec1 | grep -v ERROR
 }
+
+function volumedown {
+    echo voldown | cec-client -s -d 1  /dev/cec0
+}
+
+function volumeup {
+    echo volup | cec-client -s -d 1  /dev/cec0
+}
+
 
 case "$1" in
 
@@ -26,7 +42,14 @@ case "$1" in
     'status')
         status
     ;;
-        *)
-        echo -e "\nUsage: monitor.sh on|off|status"
+    'volumeup')
+        volumeup
+    ;;
+    'volumedown')
+        volumedown
+    ;;
+
+     *)
+        echo -e "\nUsage: monitor.sh on|off|status|volumeup|volumedown"
     ;;
     esac
